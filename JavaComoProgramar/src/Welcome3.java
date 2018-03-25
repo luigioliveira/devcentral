ccc4d8c3dbdf137e8ca44ae14358c25fb2e2116f
LuigiDarcoGianizelli <luigi.gianizelli@gmail.com>
2018-03-24 21:40:20 -0300

    Update Welcome3.java
    Adicionar um trecho no código para recebe-lo via pull no Eclipse.

--- JavaComoProgramar/src/Welcome3.java
+++ JavaComoProgramar/src/Welcome3.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Welcome3 {
 
 	public static void main(String[] args) {
+		// Pull do Github. 
 		int weight; //peso
 		double height; //altura
 		
@@ -19,7 +20,7 @@ public class Welcome3 {
 		
 		System.out.printf("\n\n%s\n%s\n%s\n%s\n%s\n","BMI VALUES","Underweight: less than 18.5","Normal: between 18.5 and 24.9","Overweight: between 25 and 29.9","Obese: 30 or greater");
 		
-
+			
 	}
 
 }
